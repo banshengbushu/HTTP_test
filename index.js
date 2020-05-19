74c10471b690b83b88cea8a2eee856b2c57aa279
Danna <wdanna_@outlook.com>
2020-05-19 14:23:04 +0800

    Upload all changes

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-const onSend = () => {
-    const xhr = new XMLHttpRequest();
-    const url_github = 'https://api.github.com/users/banshengbushu';
+class AudioPlayer {
+    constructor() {
+        this.container = document.querySelector(container);
 
-    xhr.open('GET', url_github);
-    xhr.setRequestHeader('X-Custom-Header', 'value');
-    xhr.send();
+        this.songList = [];  // 歌单列表
+        this.dom = null;    // 用于存放dom
+        this.audio = new Audio(); // Audio对象
 
-    var url_alice = 'http://api.alice.com/cors';
-    xhr.open('PUT', url_alice, true);
-    xhr.setRequestHeader('X-Custom-Header', 'value');
-    xhr.send();
-};
+        this.getSongs();
+        this.createElement();
+        this.bindEvents();
+        this.render();
+    }
 
+    getSongs() {
+        // ...ajax......
+        this.songs = [
+            {
+                cover: '', // 封面
+                url: '.mp3', // 资源地址
+                singer: {}, // 歌手的信息
+                name: '' // 歌曲的名字
+            }
+        ];
+    }
+
+    createElement() {
+        // 生成dom
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <div class="btn">播放按钮</div>
+            <div>进度条</div>
+        `;
+
+        this.dom = div;
+    }
+
+    bindEvents() {
+        this.dom.querySelector('.btn').addEventListener('click', () => {    
+            console.log('开始播放');
+        });
+    }
+
+    render() {
+        this.container.appendChild(this.dom);
+    }
+}
+
+new AudioPlayer('#app');
